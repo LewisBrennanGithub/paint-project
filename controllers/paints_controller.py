6240a7121d8e621315a3b02b22c57ec7f09f5af1
Lewis Brennan <lewisbrennan444@gmail.com>
2023-03-24 22:50:15 +0000

    next

--- controllers/paints_controller.py
+++ controllers/paints_controller.py
@@ -4,3 +4,13 @@ from models.paint import Paint
 import repositories.paint_repository as paint_repository
 
 paints_blueprint = Blueprint("paints", __name__)
+
+@paints_blueprint.route("/paints")
+def users():
+    paints = paint_repository.select_all() # NEW
+    return render_template("paints/index.html", paints = paints)
+
+@paints_blueprint.route("/paints/<id>")
+def show(id):
+    paint = paint_repository.select(id)
+    return render_template("paints/show.html", paint=paint)
